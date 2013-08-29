50a7745c73c3ee19ac4edeb18fc922ff9b93c49e
Sami Luukkonen <sami@devlab.fi>
2013-08-29 17:35:20 +0300

    tarkista_tehtaan_saldot js

--- js/tilaus_myynti/tilaus_myynti.js
+++ js/tilaus_myynti/tilaus_myynti.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	bind_valitut_rivit_checkbox_click();
+	bind_tarkista_tehtaan_saldot_click();
 });
 
 function bind_valitut_rivit_checkbox_click() {
@@ -19,8 +20,8 @@ function bind_valitut_rivit_checkbox_click() {
 		else {
 			if ($(this).val().indexOf(',') > -1) {
 				tunnukset_array = $(this).val().split(',');
-				for (var i in tunnukset_array) {
-					$("#jyvita_valitut_form input[value='" + tunnukset_array[i] + "']").remove();
+				for (var x in tunnukset_array) {
+					$("#jyvita_valitut_form input[value='" + tunnukset_array[x] + "']").remove();
 				}
 			}
 			else {
@@ -34,4 +35,46 @@ function nappi_onclick_confirm(message) {
 	ok = confirm(message);
 
 	return ok;
+}
+
+function bind_tarkista_tehtaan_saldot_click() {
+
+	$('div.availability').css({
+		'width': '15px',
+		'height': '15px',
+		'float': 'left',
+		'background-color': 'transparent',
+		'margin-left': '5px',
+		'border-radius': '50%',
+		'-webkit-border-radius': '50%',
+		'-moz-border-radius': '50%'
+	});
+
+	var bgcolors = ['#E66','#FCF300','#5D2'];
+
+	$('.tarkista_tehtaan_saldot').on('click', function() {
+
+		$id = $(this).attr('id');
+		$otunnus = $('#tilausnumero').val();
+		$tuoteno = $('#'+$id+'_tuoteno').val();
+
+		$('#'+$id+'_loading').html("<img src='../pics/loading_blue_small.gif' />");
+
+		$.post('',
+			{
+			id: $id,
+			otunnus: $otunnus,
+			tuoteno: $tuoteno,
+			ajax_toiminto: 'tarkista_tehtaan_saldot',
+			no_head: 'yes',
+			ohje: 'off'
+			},
+			function(return_value) {
+				var data = jQuery.parseJSON(return_value);
+				$('#'+data.id+'_loading').html('');
+
+				$('#'+data.id+'_availability').css({'background-color': bgcolors[data.saldo[0]]}).show();
+			}
+		);
+	});
 }
